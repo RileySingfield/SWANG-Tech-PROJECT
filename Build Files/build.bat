65a8f1f62bd4ba1fb86dbb8cb1c6209e4c9572a4
Aminah Sumaiya <aminahsumaiya@192.168.1.28>
2025-04-04 11:30:04 -0400

    Added build and run files for Windows

---
+++ Build Files/build.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Building the BeautyCatalog project...
+mvn clean compile
+echo Build complete!
+pause
---
+++ Build Files/run.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Running the BeautyCatalog application...
+mvn exec:java
+pause
